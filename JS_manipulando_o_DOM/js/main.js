75208815ff5b2d5b32db5ad1e6073759a9dc9d2f
Vini Antunes <vinicius.antunes@dp6.com.br>
2022-07-07 16:05:19 -0300

    refactor: Adiciona cntroles dinâmicos

--- JS_manipulando_o_DOM/js/main.js
+++ JS_manipulando_o_DOM/js/main.js
@@ -1,22 +1,25 @@
 const subtrair = document.querySelector("#subtrair");
 const somar = document.querySelector("#somar");
-const braco = document.querySelector("#braco");
+const controle = document.querySelectorAll(".controle-ajuste");
+
+controle.forEach( (elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        let operacao = evento.target.textContent;
+        let controle = evento.target.parentNode;
+        manipulaDados(operacao, controle);
+    });
+});
+
+function manipulaDados(operacao, controle) {
+    let peca = controle.querySelector(".controle-contador");
+
+    if(operacao == "-") {
+        peca.value = parseInt(peca.value) - 1;
+    } else if(operacao == "+") {
+        peca.value = parseInt(peca.value) + 1;
+    };
+};
 
 robotron.addEventListener("click", () => {
     console.log("Click no robô");
 });
-
-function dizOi(nome) {
-    console.log("Oi " + nome);
-    console.log("Bem vindo ao Robotron 2000");
-};
-
-dizOi('Nina');
-
-somar.addEventListener("click", soma());
-
-function soma() {
-    const valor = parseInt(document.querySelector("#braco").getElementsByClassName("controle-contador").value[0]);
-    valor.value += 1;
-    console.log(valor.value); 
-};
