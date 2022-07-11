99ff1065165bf118579b01aeae65a736d0b749cb
Vini Antunes <vinicius.antunes@dp6.com.br>
2022-07-11 11:09:21 -0300

    feat: Adiciona cálcuos dinâmicos

--- JS_manipulando_o_DOM/js/main.js
+++ JS_manipulando_o_DOM/js/main.js
@@ -1,13 +1,53 @@
 const subtrair = document.querySelector("#subtrair");
 const somar = document.querySelector("#somar");
 const controle = document.querySelectorAll("[data-controle]");
+const estatisticas = document.querySelectorAll("[data-estatistica]");
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+};
 
 controle.forEach( (elemento) => {
     elemento.addEventListener("click", (evento) => {
         let operacao = evento.target.dataset.controle;
         let controle = evento.target.parentNode;
+        let peca = evento.target.dataset.peca;
         manipulaDados(operacao, controle);
+        atualizaEstatisticas(peca);
     });
+});
+
+robotron.addEventListener("click", () => {
+    console.log("Click no robô");
 });
 
 function manipulaDados(operacao, controle) {
@@ -20,6 +60,9 @@ function manipulaDados(operacao, controle) {
     };
 };
 
-robotron.addEventListener("click", () => {
-    console.log("Click no robô");
-});
+function atualizaEstatisticas(peca) {   
+    estatisticas.forEach( (elemento) => {
+        let item = elemento.dataset.estatistica;
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][item];
+    });
+};
